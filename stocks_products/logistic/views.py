ac85bf904da3b1e2b9ac760e2bb4dcac04fdf13d
Захаров Сергей Дмитриевич <zaharovsd@tgmu.tyumsmu.ru>
2023-09-27 17:12:17 +0500

    second commit

---
+++ stocks_products/logistic/views.py
@@ -0,0 +1,24 @@
+from rest_framework.viewsets import ModelViewSet
+
+from logistic.models import Product, Stock
+from logistic.serializers import ProductSerializer, StockSerializer
+
+from rest_framework.filters import SearchFilter
+
+class CustomSearchFilter(SearchFilter):
+    search_param = 'products'
+
+class ProductViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
+
+
+class StockViewSet(ModelViewSet):
+    queryset = Stock.objects.all()
+    serializer_class = StockSerializer
+    # при необходимости добавьте параметры фильтрации
+    filter_backends = [CustomSearchFilter]
+    search_fields = ['products__title', 'products__description']
